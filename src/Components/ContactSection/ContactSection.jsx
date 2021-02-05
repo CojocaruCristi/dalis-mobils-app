6a81f0593496d0b22fce581c56f67d364667bba2
unknown <cojocarucristiandev@gmail.com>
2021-02-05 02:08:40 +0200

    done footer

--- src/Components/ContactSection/ContactSection.jsx
+++ src/Components/ContactSection/ContactSection.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import emailjs from 'emailjs-com';
+
 
 import styledClasses from "./ContactSection.module.css"
 import {Parallax} from "react-parallax";
 import contactSectionBG from "./../../Img/contactSectionBG.jpeg"
+import {Button, Form} from "react-bootstrap";
 
 
 export const ContactSection = () => {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_nc9ha6m', 'template_i99rr8m', e.target, 'user_YaJThzncJIJHUesJCjfjc')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
 
     return (
         <div id={"contact"}>
@@ -17,7 +32,20 @@ export const ContactSection = () => {
                       strength={300}
             >
                 <div style={{height: "110vh"}}>
-
+                    <div className={styledClasses.formContainer}>
+                        <form className="contact-form" onSubmit={sendEmail}>
+                            <input type="hidden" name="contact_number" />
+                            <label>Name</label>
+                            <input type="text" name="user_name" />
+                            <label>Email</label>
+                            <input type="email" name="user_email" />
+                            <label>Number</label>
+                            <input type="text" name={"user_number"}/>
+                            <label>Message</label>
+                            <textarea name="message" />
+                            <input type="submit" value="Send" />
+                        </form>
+                    </div>
                 </div>
             </Parallax>
         </div>)
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,19 +1,109 @@
 import React from "react";
 import styledClasses from "./footer.module.css";
-import {Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Container, OverlayTrigger, Row, Tooltip} from "react-bootstrap";
+import {AiOutlineMail, BsPhone, FiFacebook} from "react-icons/all";
+
+
+const workDay = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+        9:00 - 18:00
+    </Tooltip>
+);
+const sunday = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+        9:00 - 15:00
+    </Tooltip>
+);
+const saturday = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+        Zi libera
+    </Tooltip>
+);
+
 
 export const Footer = () => {
     return (
-            <Container fluid className={styledClasses.footerSection}>
-                <Row>
-                    <Col>
-                      <a href={"tel:+373 692 56 001"}>+373 692 56 001</a><br/>
-                      <a href={"mailto:dalismobila@gmail.com"}>dalismobila@gmail.com</a>
-                    </Col>
-                    <Col>
+        <Container fluid className={styledClasses.footerSection}>
+            <Row>
+                <Col sm={"12"} md={"6"} className={styledClasses.footerBlocks}>
+                    <div>
+                        <h5>Contacte</h5>
+                        <div className={styledClasses.infoBlock}>
+                            <BsPhone/><span>Tel:</span><a href={"tel:+373 692 56 001"}>+373 692 56 001</a>
+                        </div>
+                        <div className={styledClasses.infoBlock}>
+                            <AiOutlineMail/><span>EMail:</span><a
+                            href={"mailto:dalismobila@gmail.com"}>dalismobila@gmail.com</a>
+                        </div>
+                        <div className={styledClasses.infoBlock}>
+                            <Button variant="secondary"><a href="https://www.facebook.com/dalisfurniture" target={"_blank"} style={{"text-decoration": "none"}} ><FiFacebook/></a></Button>
 
-                    </Col>
-                </Row>
-            </Container>
+                        </div>
+                    </div>
+                </Col>
+                <Col sm={"12"} md={"6"} className={styledClasses.footerBlocksDays}>
+
+                    <h5>Orarul de lucru</h5>
+
+                    <OverlayTrigger
+                        placement="top"
+                        delay={{show: 250, hide: 400}}
+                        overlay={workDay}
+
+                    >
+                        <Button className={styledClasses.wDays} variant="success">L</Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="top"
+                        delay={{show: 250, hide: 400}}
+                        overlay={workDay}
+
+                    >
+                        <Button className={styledClasses.wDays} variant="success">M</Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="top"
+                        delay={{show: 250, hide: 400}}
+                        overlay={workDay}
+
+                    >
+                        <Button className={styledClasses.wDays} variant="success">MR</Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="top"
+                        delay={{show: 250, hide: 400}}
+                        overlay={workDay}
+
+                    >
+                        <Button className={styledClasses.wDays} variant="success">J</Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="top"
+                        delay={{show: 250, hide: 400}}
+                        overlay={workDay}
+
+                    >
+                        <Button className={styledClasses.wDays} variant="success">V</Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="top"
+                        delay={{show: 250, hide: 400}}
+                        overlay={sunday}
+
+                    >
+                        <Button className={styledClasses.wDays} variant="warning">S</Button>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="top"
+                        delay={{show: 250, hide: 400}}
+                        overlay={saturday}
+
+                    >
+                        <Button className={styledClasses.wDays} variant="danger">D</Button>
+                    </OverlayTrigger>
+
+                </Col>
+            </Row>
+        </Container>
     )
 }
